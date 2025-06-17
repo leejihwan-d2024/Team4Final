e2770891c8c22e509d1e5812c4fc6ae44d524f66
tpwhd3377 <tpwhd3377@gmail.com>
2025-06-17 15:41:09 +0900

    업적 폴더 생성

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./mainpage/MainPage";
+import Achv from "./achv/Achv";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/main" element={<MainPage />} />
+        <Route path="/achv" element={<Achv />} />
         <Route path="" element={"None"} />
       </Routes>
     </BrowserRouter>
---
+++ my-app/src/achv/Achv.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Achv() {
+  return <div className="App">업적페이지입니다다 내용</div>;
+}
+
+export default Achv;
