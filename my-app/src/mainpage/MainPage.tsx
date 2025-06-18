03e37ec5320febb778b6c727f5874f5c1e773d5c
leejihwan-d2024 <leejihwan9909@gmail.com>
2025-06-18 12:31:07 +0900

    main draft

--- my-app/src/mainpage/MainPage.tsx
+++ my-app/src/mainpage/MainPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import "../App.css";
 
 function MainPage() {
   const [getRunning, setRunning] = useState(-1);
@@ -64,6 +65,16 @@ function MainPage() {
           </li>
         </ul>
       </div>
+
+      <button
+        type="button"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        onClick={function () {
+          alert("커뮤니티");
+        }}
+      >
+        (커뮤니티버튼)
+      </button>
     </div>
   );
 }
