b0f3110e26d730d45860c33451b6641c545ec143
shinjunny <shinjun0614@naver.com>
2025-07-29 11:22:35 +0900

    07291122
    스웨거 첨부

--- my-app/src/adminpage/AdminPage_Status.tsx
+++ my-app/src/adminpage/AdminPage_Status.tsx
@@ -16,9 +16,18 @@ function AdminPage_Status() {
         path={`${process.env.REACT_APP_API_BASE_URL}savemeasure`}
         title="측정 저장(헤더 없이)"
       />
-
       <span>로그인 기능 관련</span>
       <CheckStatus path="/api/auth/login" title="로그인(헤더 없이)" />
+      <span>크루 기능 관련</span>
+      <CheckStatus path="/api/crew-members/exists" />
+      <CheckStatus path="/api/crews/defaultId" />
+      <CheckStatus path="/api/crews/{id}" />
+      <CheckStatus path="/api/crews/getrecentjoin/{userId}" />
+      <CheckStatus path="/api/crews/getrecentcreate/{userId}" />
+      <CheckStatus path="/api/crews/joined" />
+      <span>채팅 기능 관련</span>
+      <CheckStatus path="/api/chatroom/{crewId}" />
+      <CheckStatus path="/api/chat/{crewId}" />
     </>
   );
 }
